283e07a633ad929aaaa620931e9871cf3c1638d9
Zhe Chen <chenlaoer82@hotmail.com>
2024-12-18 11:39:43 +0800

    修改位置管理器，NameToLocationDict对外部类为只读

--- Assets/Scripts/LocationManager.cs
+++ Assets/Scripts/LocationManager.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 public class LocationManager : Singleton<LocationManager>
 {
     // 如果不同关卡，还需在不同关卡载入卸载时改变
-    [ReadOnly] public List<GameObject> LocationList; // 单纯为调试可视化用
-    public Dictionary<string, GameObject> NameToLocationDict;
+    [ReadOnly] [SerializeField] private List<GameObject> locationList; // 单纯为调试可视化用
+
+    private Dictionary<string, GameObject> nameToLocationDict;
+
+    [ReadOnly] public Dictionary<string, GameObject> NameToLocationDict 
+    {
+        get => nameToLocationDict;
+    }
 
     private void Start()
     {
-        LocationList = new List<GameObject>();
-        NameToLocationDict = new Dictionary<string, GameObject>();
+        locationList = new List<GameObject>();
+        nameToLocationDict = new Dictionary<string, GameObject>();
         foreach (Transform child in transform)
         {
             GameObject location = child.gameObject;
             NameToLocationDict.Add(location.name, location);
-            LocationList.Add(location);
+            locationList.Add(location);
         }
     }
 }
